15960c30e7fa7d29e5772452ad98ca4026809854
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-09 17:53:51 +0000

    Fix sample text

--- src/test/java/com/ironhack/cicddemo/demoTest.java
+++ src/test/java/com/ironhack/cicddemo/demoTest.java
@@ -7,14 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class demoTest {
     private final Demo demo = new Demo();
 
-
-    // Invalid Test
     @Test
     void testGetSampleText1() {
         assertEquals("Hello!", demo.getSampleText());
     }
 
-    // Valid Test
     @Test
     void testGetSampleText2() {
         demo.setSampleText("New sample text");
