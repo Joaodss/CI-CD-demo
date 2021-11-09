88bfae9146b93c83a2d973e1fea92562a74c98a5
João Afonso Silva <joaoafonsodss@gmail.com>
2021-11-09 17:03:21 +0000

    First commit

---
+++ src/main/java/com/ironhack/cicddemo/Demo.java
@@ -0,0 +1,14 @@
+package com.ironhack.cicddemo;
+
+public class Demo {
+    private String sampleText = "Hello World!";
+
+    public String getSampleText() {
+        return sampleText;
+    }
+
+    public void setSampleText(String sampleText) {
+        this.sampleText = sampleText;
+    }
+
+}
